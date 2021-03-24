bc8ac56a9a95483cdf13cd71a3a5594d423a597e
Hany Shaalan <hshaalan@gmail.com>
2021-03-24 13:17:01 -0400

    made headway with local storage, get items from local storage and painting search history and now building function to research that city from history when clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,45 +16,99 @@ let uviRequestUrlDraft = 'http://api.openweathermap.org/data/2.5/uvi?';
 let uviRequestUrlFinal = '';
 let requestUrlStart = 'http://api.openweathermap.org/data/2.5/forecast?q=';
 let requestUrlEnd= '&units=imperial&appid=f05a2f8113996a536e6a20f88d375781';
-let history = [];
+let cityHistory = [];
 
-//function to capture city entered by user
+//get the last 10 city search history items from localstorage if it exists
+function init() {
+  let getHistory = JSON.parse(localStorage.getItem("City"))
+  if (getHistory !== null) {
+    //succeedeed in geting city history from local storage and adding to history element as bootstrap list items. I need a for loop and need to figure out a way to set max history at 
+    let getHistory0 = (getHistory[0])
+    if (getHistory0) {
+      let paintHistoryLi = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory0+'</button>')
+    }
+    
+    let getHistory1 = getHistory[1]
+    if (getHistory1) {
+      let paintHistoryLi1 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory1+'</button>')
+    }
+    
+    let getHistory2 = getHistory[2]
+    if (getHistory2) {
+      let paintHistoryLi2 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory2+'</button>')
+    }
+
+    let getHistory3 = getHistory[3]
+    if (getHistory3) {
+      let paintHistoryLi3 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory3+'</button>')
+    }
+
+    let getHistory4 = getHistory[4]
+    if (getHistory4) {
+      let paintHistoryLi4 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory4+'</button>')
+    }
+
+    let getHistory5 = getHistory[5]
+    if (getHistory5) {
+      let paintHistoryLi5 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory5+'</button>')
+    }
+    
+    let getHistory6 = getHistory[6]
+    if (getHistory6) {
+      let paintHistoryLi6 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory6+'</button>')
+    }
+
+    let getHistory7 = getHistory[7]
+    if (getHistory7) {
+      let paintHistoryLi7 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory7+'</button>')
+    }
+
+    let getHistory8 = getHistory[8]
+    if (getHistory8) {
+      let paintHistoryLi8 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory8+'</button>')
+    }
+
+    let getHistory9 = getHistory[9]
+    if (getHistory9) {
+      let paintHistoryLi9 = $('#history').prepend('<button class="btn2 btn-secondary">'+getHistory9+'</button>')
+    }
+
+    //function to capture value of city search history button clicked so we run getApi again for that city
+    $('.btn2').on('click', function () {
+      let text = $(this).text();
+      console.log(text);
+      })
+
+    }
+  }
+
+
+//function triggered when search icon is clicked when triggers getapi function
 let formSubmitHandler  = function(event) {
   event.preventDefault();
   let selectCity = nameInputEl.value.trim();
   if(selectCity) {
     nameInputEl.value = '';
   } else {
-    console.log("You must enter a city name or select from auto complete list")
+    alert("You must enter a city name")
   }
-    //preparing for local storage. First capturing all previous city entires into an array. Next will test populating to history element and once successful will save to local storage then populate history with a for loop
-    history.push(selectCity);
-    console.log(history);
-    // historyEl.append(selectCity)
-    // let historyNew = document.createElement("br")
-    // historyEl.appendChild(historyNew)
    
-    //using localstorage.setitem to store entire array to local storage. it worked !!!
-    localStorage.setItem("history", history)
-    ////now use a get item function to pull local storage date and populate into history element
-
-
-    
-      // historyEl.append(selectCity)
-    // let historyNew = document.createElement("br")
-    // historyEl.appendChild(historyNew)
+  //constructing URL using 2 constants and 1 variable 
   let requestUrl = (requestUrlStart + selectCity + requestUrlEnd);
   getApi();
 
-  function getApi() {
-    // let requestUrl = "https://api.openweathermap.org/data/2.5/forecast?q=fairfax,va,us&units=imperial&appid=f05a2f8113996a536e6a20f88d375781"
+  //LOCAL STORAGE: store last city in an array and seperately last url in another array
+  let saveLastCity = cityHistory.push(selectCity)
+  localStorage.setItem("City", JSON.stringify(cityHistory));
 
+  function getApi() {
     fetch(requestUrl)
       .then(function (response) {
           return response.json();
       }
       )
       .then(function (data) {
+        console.log(data);
 
           //capturing current day's city name, temp, humidity,and wind speed then populating to html element 
           cityName.textContent = (data.city.name + ", " + data.city.country);
@@ -62,55 +116,72 @@ let formSubmitHandler  = function(event) {
           humidity.textContent = ("Humidity: " + data.list[0].main.humidity + " %");
           wind.textContent = ("Wind Speed: " + data.list[0].wind.speed + " MPH");
 
-          //appending the correct image file to the icon element. I would have to write 18 if statements to get the correct icon image based on the icon value returned from the api. Scheduled tutor tomorrow 3/23/21. 
-          if (data.list[0].weather[0].icon === "02d" ) {
-            $('#icon').append($('<img src="http://openweathermap.org/img/wn/01d.png" alt="Sunny"></img>'))
-          }
-          else {
-            $('#icon').append($('<img src="http://openweathermap.org/img/wn/02d.png" alt="Sunny"></img>'))  
-          }
+          //adding icon
+          $('#icon').empty()
+          let image = $('<img>')
+          image.attr('src', `http://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`)
+          $('#icon').append(image)
 
           //capturing city's long and lat and storing in global varible to later run a second api call to get UV index
           cityLat = data.city.coord.lat
           cityLon =  data.city.coord.lon
           uviRequestUrlFinal = (uviRequestUrlDraft + 'lat=' + cityLat + '&lon=' + cityLon + '&appID=' + appId)
 
-          //capturing next day's date, temp, humidity
-          //first convert dt_text format using moment to friendly format mm/dd/yy
+          //capturing next day's date, temp, humidity. Converting date format using moment
           let day1Date = moment(data.list[2].dt_txt).format("MM/DD/YYYY")
           $('#day1-date').text(day1Date);
-          //To DO: add icon
+          //adding icon
+          $('#day1-icon').empty()
+          let image1 = $('<img>')
+          image1.attr('src', `http://openweathermap.org/img/wn/${data.list[6].weather[0].icon}.png`)
+          $('#day1-icon').append(image1)
           $('#day1-temp').text("Temp: " + data.list[2].main.temp + " °F");
           $('#day1-humidity').text("Humidity: " +data.list[2].main.humidity + " %");
-         //capturing second day's date, temp, humidity
-         //first convert dt_text format using moment to friendly format mm/dd/yy
+
+        //capturing 2nd day's date, temp, humidity. Converting date format using moment
           let day2Date = moment(data.list[10].dt_txt).format("MM/DD/YYYY")
           $('#day2-date').text(day2Date);
-          //To DO: add icon
+          //adding icon
+          $('#day2-icon').empty()
+          let image2 = $('<img>')
+          image2.attr('src', `http://openweathermap.org/img/wn/${data.list[14].weather[0].icon}.png`)
+          $('#day2-icon').append(image2)
           $('#day2-temp').text("Temp: " + data.list[10].main.temp + " °F");
           $('#day2-humidity').text("Humidity: " +data.list[10].main.humidity + " %");
-         //capturing third day's date, temp, humidity
-         //first convert dt_text format using moment to friendly format mm/dd/yy
+
+         //capturing 3rd day's date, temp, humidity. Converting date format using moment
           let day3Date = moment(data.list[18].dt_txt).format("MM/DD/YYYY")
           $('#day3-date').text(day3Date);
-          //To DO: add icon
+          //adding icon
+          $('#day3-icon').empty()
+          let image3 = $('<img>')
+          image3.attr('src', `http://openweathermap.org/img/wn/${data.list[20].weather[0].icon}.png`)
+          $('#day3-icon').append(image3)
           $('#day3-temp').text("Temp: " + data.list[18].main.temp + " °F");
           $('#day3-humidity').text("Humidity: " +data.list[18].main.humidity + " %");
-         //capturing fourth day's date, temp, humidity
-         //first convert dt_text format using moment to friendly format mm/dd/yy
+
+         //capturing 4th day's date, temp, humidity. Converting date format using momenty
           let day4Date = moment(data.list[26].dt_txt).format("MM/DD/YYYY")
           $('#day4-date').text(day4Date);
-          //To DO: add icon
+          //adding icon
+          $('#day4-icon').empty()
+          let image4 = $('<img>')
+          image4.attr('src', `http://openweathermap.org/img/wn/${data.list[30].weather[0].icon}.png`)
+          $('#day4-icon').append(image4)
           $('#day4-temp').text("Temp: " + data.list[26].main.temp + " °F");
           $('#day4-humidity').text("Humidity: " +data.list[26].main.humidity + " %");
-         //capturing fifth day's date, temp, humidity
-         //first convert dt_text format using moment to friendly format mm/dd/yy
+
+         //capturing 5th day's date, temp, humidity. Converting date format using moment
           let day5Date = moment(data.list[34].dt_txt).format("MM/DD/YYYY")
           $('#day5-date').text(day5Date);
-          //To DO: add icon
+          //adding icon
+          $('#day5-icon').empty()
+          let image5 = $('<img>')
+          image5.attr('src', `http://openweathermap.org/img/wn/${data.list[38].weather[0].icon}.png`)
+          $('#day5-icon').append(image5)
           $('#day5-temp').text("Temp: " + data.list[34].main.temp + " °F");
           $('#day5-humidity').text("Humidity: " +data.list[34].main.humidity + " %");
-          //function to populate icon image based on icon value returned
+          
       } )
 
       //making second api call to get the uvi index based on the lon and lat we captured earlier
@@ -124,7 +195,7 @@ let formSubmitHandler  = function(event) {
           uvIndex.textContent = (data.value)
           let uvValue = data.value;
           
-          //now styling the UV element with apporpriate color codes
+          // styling the UV Index element with apporpriate color codes
           if ((uvValue > 0) && (uvValue <= 2.99)) {
             $('#uv-value').css('background-color', 'green')
             $('#uv-value').css('color', 'white')
@@ -149,56 +220,20 @@ let formSubmitHandler  = function(event) {
             ) 
     }
       )
-      
-      //running the function to display today's date using moment.js format
+      //calling function to display today's date using moment.js format
       displayToday();
 };
 }
 
-
-
-//function call weather api
-
-
-//event listener to fire up api function upon clicking search 
-// searchBtn.addEventListener('click', getApi);
+//event listener to fire main function which has all other functions in it
 searchBtn.addEventListener('click', formSubmitHandler);
 
-//display current date using moment.js 
+//calling functions to set and get items in local storage
+init();
+
+//defining function to display current date using moment.js 
 function displayToday (event) {
     // event.preventDefault();
     let todayDate = moment().format('dddd, MMM DD, YYYY');
     today.textContent = (" (" + todayDate) +")";
     };
-
-
-//jquery auto complete function. Not necessary, maybe we will add major cities
-// $( function() {
-//   var availableCities = [
-//     "ActionScript",
-//     "AppleScript",
-//     "Asp",
-//     "BASIC",
-//     "C",
-//     "C++",
-//     "Clojure",
-//     "COBOL",
-//     "ColdFusion",
-//     "Erlang",
-//     "Fortran",
-//     "Groovy",
-//     "Haskell",
-//     "Java",
-//     "JavaScript",
-//     "Lisp",
-//     "Perl",
-//     "PHP",
-//     "Python",
-//     "Ruby",
-//     "Scala",
-//     "Scheme"
-//   ];
-//   $( "#cityname" ).autocomplete({
-//     source: availableCities
-//   });
-// } );
